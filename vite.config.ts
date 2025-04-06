ffb57d4ec13db6f4e81031efe283684d5fbcd54c
mathiasstamm <matze0923@gmail.com>
2025-04-06 22:25:29 +0200

    bump node version

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite';
-import {nodePolyfills} from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [
-    nodePolyfills({
-      // Enable polyfills for Node.js modules like `crypto`
-      protocolImports: true,
-    }),
-  ],
   build: {
     target: 'esnext', // Ensure modern syntax is supported
     rollupOptions: {
